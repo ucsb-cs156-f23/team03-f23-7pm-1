d01306d284ff2d97636562cf3b946484cc44cbbf
Carson Kopper <ckopper@ucsb.edu>
2023-11-02 15:31:37 -0700

    ck - added UCSBDiningCommonsMenuItem fixtures on frontend

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemsFixtures.js
@@ -0,0 +1,38 @@
+const restaurantFixtures = {
+    oneUcsbDiningCommonsMenuItem:
+    [
+        {
+            "id": 1,
+            "name": "Cheese Pizza",
+            "station": "Pizza",
+            "diningCommonsCode": "carillo"
+        }
+    ],
+
+    threeUcsbDiningCommonsMenuItems:
+    [
+        {
+            "id": 2,
+            "name": "Beef Stir Fry",
+            "station": "Mongolian Grill",
+            "diningCommonsCode": "carillo"
+        },
+
+        {
+            "id": 3,
+            "name": "Tres Leches Cake",
+            "station": "Bakery",
+            "diningCommonsCode": "de-la-guerra" 
+        },
+
+        {
+            "id": 4,
+            "name": "Fettucine Alfredo",
+            "station": "The Brick",
+            "diningCommonsCode": "portola"     
+        },
+        
+    ]
+};
+
+export { restaurantFixtures };
