14a1f1da30b187e2f09aef66ba63c0a759a67f50
josephyue <josephyue@ucsb.edu>
2023-11-12 13:59:30 -0800

    Revert "Update MenuItemReviewEditPage.test.js"
    This reverts commit 803d2c61596427b5b5b921e32eaec371c3d30173.
    
    revert

--- frontend/src/tests/pages/MenuItemReview/MenuItemReviewEditPage.test.js
+++ frontend/src/tests/pages/MenuItemReview/MenuItemReviewEditPage.test.js
@@ -173,6 +173,11 @@ describe("MenuItemReviewEditPage tests", () => {
 
             await waitFor(() => expect(mockToast).toBeCalled());
             expect(mockNavigate).toBeCalledWith({ "to": "/ucsbmenuitemreview" });
+        });
+
+
+    });
+});
 
             expect(idField).toBeInTheDocument();
             expect(idField).toHaveValue("2");
