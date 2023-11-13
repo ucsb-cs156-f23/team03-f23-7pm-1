4d02966583eb113d1b25c5dc67133deb027c87b5
daniel-li2021 <zeyangli@ucsb.edu>
2023-11-12 19:48:23 -0800

    Revert "Create Page for RecommendationRequest plus tests and stories#31"

--- frontend/src/main/pages/RecommendationRequest/RecommendationRequestCreatePage.js
+++ frontend/src/main/pages/RecommendationRequest/RecommendationRequestCreatePage.js
@@ -1,52 +1,12 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-import RecForm from "main/components/RecommendationRequest/RecommendationRequestForm";
-import { Navigate } from 'react-router-dom'
-import { useBackendMutation } from "main/utils/useBackend";
-import { toast } from "react-toastify";
 
-export default function RecommendationRequestCreatePage({storybook=false}) {
+export default function RecommendationRequestCreatePage() {
 
-  const objectToAxiosParams = (request) => ({
-    url: "/api/recommendationrequests/post",
-    method: "POST",
-    params: {
-      requesterEmail: request.requesterEmail,
-      professorEmail: request.professorEmail,
-      explanation: request.explanation,
-      dateRequested: request.dateRequested,
-      dateNeeded: request.dateNeeded,
-      done: request.done
-    }
-  });
-
-  const onSuccess = (request) => {
-    toast(`New recommendation request created - id: ${request.id}`);
-  }
-
-  const mutation = useBackendMutation(
-    objectToAxiosParams,
-     { onSuccess }, 
-     // Stryker disable next-line all : hard to set up test for caching
-     ["/api/recommendationrequests/all"]
-     );
-
-  const { isSuccess } = mutation
-
-  const onSubmit = async (data) => {
-    mutation.mutate(data);
-  }
-
-  if (isSuccess && !storybook) {
-    return <Navigate to="/recommendationrequests" />
-  }
-
+  // Stryker disable all : placeholder for future implementation
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Create New Recommendation Request</h1>
-
-        <RecForm submitAction={onSubmit} />
-
+        <h1>Create page not yet implemented</h1>
       </div>
     </BasicLayout>
   )
